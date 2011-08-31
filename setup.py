63fc7b0fee95ea4f60864426a47db854bf0c65f0
taericks <taericks@precisionm6500b>
2011-08-31 15:12:59 -0700

    updated version to 0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0.4'
+version = '0.1.0'
 
 setup(
     name='pykml',
@@ -43,117 +43,16 @@ setup(
     license='BSD',
     long_description="""\
 =========
-PyKML
+pyKML
 =========
-PyKML is a Python package for parsing and authoring KML documents. It is based
+pyKML is a Python package for parsing and authoring KML documents. It is based
 on the lxml.objectify API (http://codespeak.net/lxml/objectify.html) which
 provides Pythonic access to XML documents.
 
-------------
-Dependencies
-------------
-* lxml (`instructions for installing lxml`_)
+.. figure:: http://pykml.googlecode.com/hg/docs/source/logo/pyKML_logo_200x200.png
+   :scale: 100 %
+   :alt: pyKML logo
 
-..  _`instructions for installing lxml`: http://notecodespeak.net/lxml/installation.html
-
-To verify that the lxml library has been installed correctly, open up a Python
-shell and type:
-
->>> import lxml
->>>
-
-------------
-Installation
-------------
-PyKML can be installed from the Python Package Index, using either easy_install
-or pip:
-
-  $ sudo easy_install pykml
-  
-or
-
-  $ sudo pip install pykml
-
-The installation can be tested by running the following:
-
-  $ nosetests -s --with-coverage
-
-------
-Usage
-------
-
-KML documents can be constructed by using element factory objects.  The
-following example uses two factory objects, corresponding to the OGC KML and
-ATOM namespaces:
-
->>> from pykml.factory import KML_ElementMaker as K
->>> from pykml.factory import ATOM_ElementMaker as ATOM
->>> doc = K.kml(
-...         K.Document(
-...           ATOM.author(
-...             ATOM.name("J. K. Rowling")
-...           ),
-...           ATOM.link(href="http://www.harrypotter.com"),
-...           K.Placemark(
-...             K.name("Hogwarts"),
-...             K.Point(
-...               K.coordinates("1,1")
-...             )
-...           )
-...         )
-...       )
-
-Constructed documents can be converted to a string representation:
-
->>> from lxml import etree
->>> etree.tostring(doc)
-
-And can be validated against the official KML XML Schema: 
-
->>> from pykml.parser import Schema
->>> print Schema('ogckml22.xsd').validate(doc)
-
-Existing KML documents can also be parsed:
-
->>> import urllib2
->>> from pykml.parser import parse
->>> url = 'http://code.google.com/apis/kml/documentation/KML_Samples.kml'
->>> fileobject = urllib2.urlopen(url)
->>> doc = parse(fileobject, schema=Schema('ogckml22.xsd'))
-
-Documents that make use of the Google Extension namespace elements can be 
-created and validate using the Google Extensions schema:
-
->>> from pykml.factory import Schema
->>> from pykml.factory import KML_ElementMaker as K
->>> from pykml.factory import ATOM_ElementMaker as ATOM
->>> from pykml.factory import GX_ElementMaker as GX
->>> schema = Schema('kml22gx.xsd')
->>> doc = K.kml(
-...       GX.Tour(
-...         GX.Playlist(
-...           GX.SoundCue(
-...             K.href("http://dev.keyhole.com/codesite/cntowerfacts.mp3")
-...           ),
-...           GX.Wait(
-...             GX.duration(10)
-...           ),
-...           GX.FlyTo(
-...             GX.duration(5),
-...             GX.flyToMode("bounce"),
-...             K.LookAt(
-...               K.longitude(-79.387),
-...               K.latitude(43.643),
-...               K.altitude(0),
-...               K.heading(-172.3),
-...               K.tilt(10),
-...               K.range(1200),
-...               K.altitudeMode("relativeToGround"),
-...             )
-...           )
-...         )
-...       )
-... )
->>> print schema.validate(doc)
+See the Package Documentation for information on installation and usage.
 """,
 )
