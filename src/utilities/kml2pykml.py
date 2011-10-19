79c9a07148ee7a7c2070b4137ebe52493b7a371c
taericks <taericks@precisionm6500b>
2011-10-19 07:21:11 -0700

    updated kml2pykml utility to use revised pyKML objects

--- src/utilities/kml2pykml.py
+++ src/utilities/kml2pykml.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 import sys
 import getopt
-from pykml.kml_gx.parser import parse
-from pykml.kml_ogc.factory import write_python_script_for_kml_document
+from pykml.parser import parse
+from pykml.parser import Schema
+from pykml.factory import write_python_script_for_kml_document
 
 class Usage(Exception):
     def __init__(self, msg):
@@ -17,9 +18,10 @@ def main(argv=None):
         except getopt.error, msg:
              raise Usage(msg)
         # main code
+        schema = Schema("kml22gx.xsd")
         filename = argv[1]
         with open(filename) as f:
-            kmldoc = parse(f, validate=True)
+            kmldoc = parse(f, schema=schema)
             print write_python_script_for_kml_document(kmldoc)
 
     except Usage, err:
