ecd1ec7d226068111475c7009beb11b2e0ce0836
elmerehbi <elmerehbi@users.noreply.github.com>
2019-03-17 17:23:49 +0100

    Fix for Python 3

--- src/pykml/parser.py
+++ src/pykml/parser.py
@@ -1,11 +1,14 @@
 '''pyKML Parser Module
 
-The pykml.parser module provides functions that can be used to parse KML 
+The pykml.parser module provides functions that can be used to parse KML
 from a file or remote URL.
 '''
 import sys
 import os
-import urllib2
+try:
+    from urllib.request import urlopen
+except ImportError:
+    from urllib2 import urlopen
 from lxml import etree, objectify
 
 OGCKML_SCHEMA = 'http://schemas.opengis.net/kml/2.2.0/ogckml22.xsd'
@@ -21,19 +24,19 @@ class Schema():
                 self.schema = etree.XMLSchema(file=f)
         except:
             # try to open a remote URL
-            f = urllib2.urlopen(schema)
+            f = urlopen(schema)
             self.schema = etree.XMLSchema(file=f)
-    
+
     def validate(self, doc):
         """Validates a KML document
-        
-        This method eturns a boolean value indicating whether the KML document 
+
+        This method returns a boolean value indicating whether the KML document
         is valid when compared to the XML Schema."""
         return self.schema.validate(doc)
-    
+
     def assertValid(self, doc):
         """Asserts that a KML document is valide
-        
+
         The method generates a validation error if the document is not valid
         when compared to the XML Schema.
         """
@@ -41,8 +44,8 @@ class Schema():
 
 def fromstring(text, schema=None):
     """Parses a KML text string
-    
-    This function parses a KML text string and optionally validates it against 
+
+    This function parses a KML text string and optionally validates it against
     a provided schema object"""
     if schema:
         parser = objectify.makeparser(schema = schema.schema)
@@ -91,7 +94,7 @@ def validate_kml():
         fileobject = open(uri)
     except IOError:
         try:
-            fileobject = urllib2.urlopen(uri)
+            fileobject = urlopen(uri)
         except ValueError:
             raise ValueError('Unable to load URI {0}'.format(uri))
     except:
@@ -119,4 +122,3 @@ def validate_kml():
         pass #variable was not defined
     else:
         fileobject.close
-    
